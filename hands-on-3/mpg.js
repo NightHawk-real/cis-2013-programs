b784fa599ac681399c1ee03721562c82e629f3fb
NightHawk-real <112212239+NightHawk-real@users.noreply.github.com>
2022-09-12 15:02:10 -0500

    Add files via upload

---
+++ hands-on-3/mpg.js
@@ -0,0 +1,6 @@
+var floatMiles, floatGallons, floatMpg;
+     floatMiles = prompt("Enter miles driven");
+     floatMiles = parseFloat
+     floatGallons = prompt("Enter gallons of gas used");
+     floatMpg = floatMiles/floatGallons;
+     alert("Miles per gallon = " + mpg);
