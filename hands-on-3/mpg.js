d8b2067feaff6968563f64da8a07f89f0bad0e71
NightHawk-real <colton.henkle@gmail.com>
2022-09-14 13:15:56 -0500

    Corrected incorrect variable reference

--- hands-on-3/mpg.js
+++ hands-on-3/mpg.js
@@ -2,4 +2,4 @@ var floatMiles, floatGallons, floatMpg;
      floatMiles = parseFloat(prompt("Enter miles driven"));
      floatGallons = parseFloat(prompt("Enter gallons of gas used"));
      floatMpg = floatMiles/floatGallons;
-     alert("Miles per gallon = " + mpg);
+     alert("Miles per gallon = " + floatMpg);
