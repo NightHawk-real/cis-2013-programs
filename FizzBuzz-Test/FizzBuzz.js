32ae197834fd9f0084a09b2881aedc99d5912113
NightHawk-real <colton.henkle@gmail.com>
2022-10-28 13:22:27 -0500

    created FizzBuzz function

--- FizzBuzz-Test/FizzBuzz.js
+++ FizzBuzz-Test/FizzBuzz.js
@@ -15,12 +15,32 @@ function whileFunction()
 {
 	var counter = 1;
 	var output = "";	
+
+	while(counter <= 100) 
+	{
+		if(counter % 15 === 0)
+		{
+			output = output + "FizzBuzz\n";
+			counter++;
+		}
+		else if(counter % 5 === 0)
+		{
+			output = output + "Buzz\n";
+			counter++;
+		}
+		else if(counter % 3 === 0)
+		{
+			output = output + "Fizz\n";
+			counter++;
+		}
+		else
+		{
+			output = output + counter + "\n";
+			counter++
+		}
+	}
+	alert(output);
 };
-
-
-
-
-
 
 window.onload = function()
 {
