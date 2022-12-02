5a01d9c561a1079803f675704a84935e6c1ec658
NightHawk-real <colton.henkle@gmail.com>
2022-12-02 14:03:25 -0600

    commented

--- phrase-manipulator/phraseStr.js
+++ phrase-manipulator/phraseStr.js
@@ -6,31 +6,31 @@ var $ = function (id)
 var vowels = function () 
     {
         var stringEntry = $("phrase").value;
-        stringEntry = stringEntry.toUpperCase();
-        if (stringEntry === "")
+        stringEntry = stringEntry.toUpperCase(); //changes the case of each character in the phrase to uppercase to standardize checks later on
+        if (stringEntry === "") // validation for the input phrase
             {
                 $("phrase").value = "";
-                alert("Please enter a phrase and select a button");
+                alert("Please enter a phrase and select a button"); 
             }
         else   
             {
-                var intVowels = 0;
-                for(intCount = 0; intCount < stringEntry.length; intCount++)
+                var intVowels = 0; //initialize intVowels variable
+                for(intCount = 0; intCount < stringEntry.length; intCount++) // loop continues as long as intCount < the length of the input phrase
                 {
-                    if((stringEntry.charAt(intCount) === "A")||(stringEntry.charAt(intCount) === "E")||(stringEntry.charAt(intCount) === "I")||(stringEntry.charAt(intCount) === "O")||(stringEntry.charAt(intCount) === "U"))
+                    if((stringEntry.charAt(intCount) === "A")||(stringEntry.charAt(intCount) === "E")||(stringEntry.charAt(intCount) === "I")||(stringEntry.charAt(intCount) === "O")||(stringEntry.charAt(intCount) === "U")) //checks whether each character is an uppercase variable or not
                     {
-                        intVowels++;
+                        intVowels++; //increments count of intVowels each time the if statement runs into a vowel
                     }
                 }
-                $("output").value = "There are " + intVowels + " vowels in the phrase above.";
+                $("output").value = "There are " + intVowels + " vowels in the phrase above."; //outputs string with intConsonants count
             }
     }
 
 var consonants = function () 
     {
-        var stringEntry = $("phrase").value;
-        stringEntry = stringEntry.toUpperCase();
-        if (stringEntry === "")
+        var stringEntry = $("phrase").value; // sets input phrase to stringEntry
+        stringEntry = stringEntry.toUpperCase();  //changes the case of each character in the phrase to uppercase to standardize checks later on
+        if (stringEntry === "")  // validation of input phrase
             {
                 $("phrase").value = "";
                 alert("Please enter a phrase and select a button");
@@ -40,7 +40,7 @@ var consonants = function ()
                 var intConsonants = 0;
                 for(intCount = 0; intCount < stringEntry.length; intCount++)
                 {
-                    switch(stringEntry.charAt(intCount))
+                    switch(stringEntry.charAt(intCount)) // switch statement to test for each of the uppercase vowels
                     {
                         case "A":
                         case "E":
@@ -48,35 +48,35 @@ var consonants = function ()
                         case "O":
                         case "U":
                         case " ":
-                            break;
-                        default:
-                            intConsonants++;
+                            break; // does not count the vowels and moves onto the next character
+                        default:   //default statement if a vowel is not caught
+                            intConsonants++; // increments the intConsonants variable when a character is not a vowel
                             break;
                     }
                 }
-                $("output").value = "There are " + intConsonants + " consonants in the phrase above."
+                $("output").value = "There are " + intConsonants + " consonants in the phrase above." // outputs string with intConsonants count
             }
     }
 var reverse = function ()
     {
         var stringEntry = $("phrase").value;
-        if (stringEntry === "")
+        if (stringEntry === "") //validation of input phrase
         {
             $("phrase").value = "";
             alert("Please enter a phrase and select a button");
         }
         else
         {
-            var stringReversed = "";
-            for(intCount = stringEntry.length - 1; intCount >= 0; intCount--)
+            var stringReversed = ""; // initializes stringReversed variable and sets it to 0
+            for(intCount = stringEntry.length - 1; intCount >= 0; intCount--) // intCount starts at the length of the input phrase, but - 1 as the condition is set to intCount >= 0. This means that the first character of stringReversed is the 9th character of stringEntry, which is at intCount = 8. It is then decremented after each character to move backwards through the phrase.
             {
-                stringReversed = stringReversed + stringEntry.charAt(intCount);
+                stringReversed = stringReversed + stringEntry.charAt(intCount); // accumulates the reversed input phrase as the loop moves backwards through the phrase
             }
-            $("output").value = "The reverse of the phrase above is: \n\"" + stringReversed + "\""
+            $("output").value = "The reverse of the phrase above is: \n\"" + stringReversed + "\"" //outputs reversed input phrase
         }
     }
 
-var clear = function () 
+var clear = function () //function clears input and output
 {
 	$("phrase").value = "";
     $("output").value = "";
@@ -85,8 +85,8 @@ var clear = function ()
 window.onload = function () 
 {
     //the following five lines of code make sure the values on the DOM are cleared.
-    $("phrase").value = "";
-    $("output").value = "";
+    $("phrase").value = ""; // sets input phrase to null onload of page
+    $("output").value = ""; // sets input phrase to null onload of page
     $("vowels").onclick = vowels; //runs the calculate_click function when clicked
     $("consonants").onclick = consonants; //runs the calculate_click function when clicked
     $("reverse").onclick = reverse; //runs the calculate_click function when clicked
